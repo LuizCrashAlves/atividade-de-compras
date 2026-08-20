11555f38a093b9ab0ad9247e8162ce5bd0f899d2
LuizCrashAlves <151794442+LuizCrashAlves@users.noreply.github.com>
2026-08-20 20:00:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const sacar = document.getElementById("saque");
 const valorSac = document.getElementById("valorSaq");
 const compra = document.getElementById("compra");
 const valorCom = document.getElementById("valorCom");
-let saldo = 100.00;
+let saldo = 0.00;
+let saldoTela = 0.00;
 let depositou;
-let valor;
+let sacou;
 let pagamento;
+let historico = [];
 
-function controleSaldo() {
+function comprar() {
     pagamento = Number(compra.value);
     if (saldo < pagamento) {
         texto.innerHTML = `Não é possivel realizar a compra! Seu saldo é: ${saldo}`;
     } else {
         saldo = (saldo - pagamento);
         texto.innerHTML = `Compra realizada! Seu novo saldo é ${saldo}`;
+        historico.push(pagamento * -1);
         return
     }
 };
@@ -25,27 +28,41 @@ function deposito() {
     depositou = Number(depositar.value);
     saldo = (saldo + depositou);
     texto.innerHTML = `seu novo saldo é: ${saldo}`
+    historico.push(depositou);
     return
 };
 
 function saque() {
     sacou = Number(sacar.value);
+    console.log(sacou);
     saldo = (saldo - sacou);
     if (saldo < 0) {
         saldo = (sacou + saldo)
         texto.innerHTML = `Não foi possivel realizar o saque, seu saldo é: ${saldo}`
     } else {
         texto.innerHTML = `seu novo saldo é: ${saldo}`
+        historico.push(sacou * -1);
     }
     return
 };
 
+function controleSaldo() {
+    for (let i = 0; i < historico.length; i++) {
+        saldoTela += historico[i];
+        console.log(historico);
+        console.log(saldoTela);
+    };
+};
+
 valorDep.addEventListener('click', () => {
     deposito();
+    controleSaldo();
 });
 valorSac.addEventListener('click', () => {
     saque();
+    controleSaldo();
 });
 valorCom.addEventListener('click', () => {
+    comprar();
     controleSaldo();
 });
